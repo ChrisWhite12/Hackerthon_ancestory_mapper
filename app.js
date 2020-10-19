6d1a000690a76cc243c29173275a5d334424cf67
ChrisWhite12 <chris_white_12@hotmail.com>
2020-10-19 10:39:28 +1000

    updated routes

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+// const MongoStore = require("connect-mongo")(session)
+const passport = require('passport');
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const dbConn = 'mongodb://localhost/ancestry_app'
+const pagesController = require('./routes/pages_routes')
+const peopleController = require('./routes/people_routes')
+const authController = require('./routes/auth_routes')
+
 app.use( express.urlencoded( {extended: false }) )
 app.use( express.json() )
 
+mongoose.connect(dbConn, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    },
+    (err) => {
+        if (err) {
+            console.log('Error connecting to database', err);
+        } else {
+            console.log('Connected to database!');
+        }
+    });
+
+    // Install middleware
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(session({
+    // resave and saveUninitialized set to false for deprecation warnings
+    secret: "Express is awesome",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1800000
+    }
+    // store: new MongoStore({
+    //     mongooseConnection: mongoose.connection
+    // })
+}));
+
+require("./middleware/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
 // use means its a middle ware
-app.use( (req, res, next) => {
-    console.log('Middle-ware running.')
-    next()
-})
+// app.use( (req, res, next) => {
+//     console.log('Middle-ware running.')
+//     next()
+// })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/pages', pagesController)
+app.use('/people', peopleController)
+app.use('/user', authController)
 
 app.listen(port, () => {
     console.log('listening on port:' + port)
